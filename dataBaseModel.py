227a0a4c966163c54b167cf0691378621ff2b72e
Ayrton Alexander Miller <23774547@student.uwa.edu.au@users.noreply.github.com>
2024-05-03 14:41:12 +0800

    This is just copies of the flask mega tutorial code basically no way it works for now

---
+++ dataBaseModel.py
@@ -0,0 +1,24 @@
+from datetime import datetime, timezone
+from typing import Optional
+import sqlalchemy as sa
+import sqlalchemy.orm as so
+from app import db
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
+from app import login
+
+class User(UserMixin, db.Model):
+    user_id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    password = db.Column(db.String(32), unique=False, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+
+class jobPostings(db.Model):
+    user_id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(32), unique=False, nullable=False)
+    jobPay = db.Column(db.Integer, unique=False, nullable=False)
+    body = db.Column(db.String(600), unique=False, nullable=False)
+
+@login.user_loader
+def load_user(id):
+    return db.session.get(User, int(id))
