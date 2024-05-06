1ac3e6567f2c95c77129057af075292329c3c7d5
huangyegithub <huangye.mailbox@gmail.com>
2024-05-06 17:50:56 +0800

    Restructure and Registration
    Restructured the files and directories to comply to the unit requirement (check out Week 7 slides, page 10).
    
    Completed the registration process so that now, data can flow into the database and users can register.

--- app.py
+++ app.py
@@ -1,23 +1,4 @@
-from flask import Flask, render_template, url_for
-
-app = Flask(__name__)
-
-@app.route('/')
-def main():
-    return render_template('main.html')
-
-@app.route('/login')
-def login():
-    return render_template('login.html')
-
-@app.route('/post')
-def post():
-    return render_template('post.html')
-
-@app.route('/register')
-def register():
-    return render_template('register.html')
+from apps import app
 
 if __name__ == '__main__':
     app.run(debug=True)
-
