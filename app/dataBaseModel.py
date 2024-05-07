fba9266fd33f5a8770272ad6be726f729a4e1ee3
Ayrton Alexander Miller <23774547@student.uwa.edu.au@users.noreply.github.com>
2024-05-07 18:58:04 +0800

    ok it actually works now I gave up trying to make the database my own and just copied the flask mega tutorial no clue why this works and my last one didn't.

--- app/dataBaseModel.py
+++ app/dataBaseModel.py
@@ -1,25 +1,39 @@
-from datetime import datetime, timezone
-from typing import Optional
-import sqlalchemy as sa
-import sqlalchemy.orm as so
-from app import db
+from datetime import datetime
+from app import db, login
+from sqlalchemy import ForeignKey
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
-from app import login
 
-class User(UserMixin, db.Model):
-    user_id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(20), unique=True, nullable=False)
-    password = db.Column(db.String(32), unique=False, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
 
-class jobPostings(db.Model):
-    post_id =db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, foreign_key=True)
-    title = db.Column(db.String(32), unique=False, nullable=False)
-    jobPay = db.Column(db.Integer, unique=False, nullable=False)
-    body = db.Column(db.String(600), unique=False, nullable=False)
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), index=True, unique=True)
+    email = db.Column(db.String(120), index=True, unique=True)
+    password_hash = db.Column(db.String(128))
+    posts = db.relationship("Post", backref="author", lazy="dynamic")
+
+    def __repr__(self):
+        return "<User {}>".format(self.username)
+
+    password_hash = db.Column(db.String(128))
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String(140))
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
+
+    def __repr__(self):
+        return "<Post {}>".format(self.body)
+
 
 @login.user_loader
 def load_user(id):
-    return db.session.get(User, int(id))
+    return User.query.get(int(id))
